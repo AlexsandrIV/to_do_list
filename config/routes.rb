6cf0d6a06c12455549ba285a44dabf15d03c5095
Alexandr <tero38@yandex.ru>
2019-04-04 03:24:37 +0300

    little bit rdit and validates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  #get 'static_pages/home'
   resources :issues
   resources :users
-  root 'users#index'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_215025) do
+ActiveRecord::Schema.define(version: 2019_04_04_000806) do
 
   create_table "issues", force: :cascade do |t|
     t.string "subject"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_04_03_215025) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
+    t.string "password_digest"
   end
 
 end
